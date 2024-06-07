5c9d1f5e559cff3172edc2937be8828328e6c94d
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2024-06-08 01:16:41 +0530

    api added

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,6 @@ import { ApiResponse } from '../types/types';
 const API_URL = 'http://localhost:3001/data';
 
 export const fetchActivityData = async () => {
-    const response = await axios.get<ApiResponse>('../db.json');
+    const response = await axios.get<ApiResponse>('./db.json');
     return response.data;
 };
